54aa09422ba5e729d2ab3a8c87de3a86d74ac92c
daikiaLucky <daikiaraki0108@gmail.com>
2021-06-15 17:14:44 +0900

    ユーザー管理機能の実装
    rails g devise:views
    viewsディレクトリの素材を移動
    「新規登録」で画面画面移動
    registrations.new.html.erbの修正
    その他諸々、、、

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum:6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
+  
+  has_many :items, dependent: :destroy
+  has_many :purchase_records, dependent: :destroy
+
 end
