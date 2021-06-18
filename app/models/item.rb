3c347007c162718739fb0d02c4990a0d5c46e79b
daikiaLucky <daikiaraki0108@gmail.com>
2021-06-18 15:50:14 +0900

    商品出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,30 +2,32 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
-  belongs_to_active_hash :shipping_chargre
+  belongs_to_active_hash :shipping_charge
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :days_to_ship
   belongs_to :user
   has_one_attached :image
-#  has_one :purchase_record
+  #  has_one :purchase_record
 
   with_options presence: true do
     validates :title
     validates :description
     validates :category_id
     validates :status_id
-    validates :shipping_chargre_id
+    validates :shipping_charge_id
     validates :prefecture_id
     validates :days_to_ship_id
     validates :price
+    validates :image
   end
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
-    validates :shipping_chargre_id
+    validates :shipping_charge_id
     validates :prefecture_id
     validates :days_to_ship_id
   end
 
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+
+    context '商品出品ができる時' do
+      it 'image, title, description, category_id, status_id, shipping_charge_id,
+         prefecture_id, deys_to_ship_id, priceが存在すれば登録できること' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品ができない時' do
+      it 'imageが空では登録できないこと' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'titleが空では登録できないこと' do
+        @item.title = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it 'descriptionが空では登録できないこと' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'categoryが空では登録できないこと' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
+      end
+
+      it 'status_idが空では登録できないこと' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Status must be other than 1')
+      end
+
+      it 'shipping_charge_idが空では登録できないこと' do
+        @item.shipping_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping charge must be other than 1')
+      end
+
+      it 'prefecture_idが空では登録できないこと' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
+      end
+
+      it 'days_to_ship_idが空では登録できないこと' do
+        @item.days_to_ship_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Days to ship must be other than 1')
+      end
+
+      it 'priceが空では登録できないこと' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが10,000,000以上では登録できないこと' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+
+      it 'priceが299以下では登録できないこと' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+
+      it 'priceが全角数字では登録できないこと' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+
+    end
+  end
 end
