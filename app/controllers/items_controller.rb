ca497342b655e70ebb1d9d3b7e851adced33bf65
daikiaLucky <daikiaraki0108@gmail.com>
2021-06-22 11:51:56 +0900

    コードレビュー２回目
    updateアクションの編集
    @itemのbefore_action化
    不必要部分コメントアウト、削除
    等

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, only: [:new, :create, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,14 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -42,8 +41,11 @@ class ItemsController < ApplicationController
                                  :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
+  def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
     redirect_to user_session_path unless user_signed_in? && current_user.id == @item.user_id
   end
 end
