7c90aede6034283f6458a6c85cfea8f585fd8fa3
daikiaLucky <daikiaraki0108@gmail.com>
2021-06-23 17:42:28 +0900

    Formオブジェクト作成

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,14 +1,27 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
-    @purchase_record = PurchaseRecord.new
-    @item = Item.find(params[:item_id]) 
+  end
+
+  def new
+    @purchase_record_shipping_address = PurchaseRecordShippingAddress.new
   end
 
   def create
+    @purchase_record_shipping_address = PurchaseRecordShippingAddress.new(purchase_recoed_params)
+    if @purchase_record_shipping_address.valid?
+      @purchase_record_shipping_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+
   def purchase_record_params
-    params.require(:purchase_record).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name,
-                                            :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_record_shipping_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :item_id).merge(user_id: current_user.id)
   end
+
 end
