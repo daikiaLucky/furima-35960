715f2e4d64b0dcf68f0a47282c3f07aefc72691e
daikiaLucky <daikiaraki0108@gmail.com>
2021-06-25 19:40:44 +0900

    環境変数処理

--- spec/models/purchase_record_spec.rb
+++ spec/models/purchase_record_spec.rb
@@ -2,26 +2,20 @@ require 'rails_helper'
 
 RSpec.describe PurchaseRecord, type: :model do
   before do
-    @purcase_record = FactoryBot.build(:purcase_record)
+    @purchase_record = FactoryBot.build(:purchase_record)
   end
 
   context '内容に問題ない場合' do
-    it 'priceとtokenがあれば保存ができること' do
-      expect(@purcase_record).to be_valid
+    it 'tokenがあれば保存ができること' do
+      expect(@purchase_record).to be_valid
     end
   end
 
   context '内容に問題がある場合' do
-    it 'priceが空では保存ができないこと' do
-      @purcase_record.price = nil
-      @purcase_record.valid?
-      expect(@purcase_record.errors.full_messages).to include("Price can't be blank")
-    end
-
     it 'tokenが空では登録できないこと' do
-      @purcase_record.token = nil
-      @purcase_record.valid?
-      expect(@purcase_record.errors.full_messages).to include("Token can't be blank")
+      @purchase_record.token = nil
+      @purchase_record.valid?
+      expect(@purchase_record.errors.full_messages).to include("Token can't be blank")
     end
   end
 end
